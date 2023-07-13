fe4823c7db3e125bbeebb374192a25accc2c17bd
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2023-07-13 12:47:12 +0200

    add pre-commit and clang format

--- any_robot_pick_n_place/include/any_robot_pick_n_place/load_parameters.hpp
+++ any_robot_pick_n_place/include/any_robot_pick_n_place/load_parameters.hpp
@@ -1,3 +1,17 @@
+// Copyright 2023 Vishnuprasad Prachandabhanu
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef LOAD_PARAMETERS_HPP
 #define LOAD_PARAMETERS_HPP
 
@@ -20,7 +34,7 @@ struct Parameters
     void load(const rclcpp::Node::SharePtr & node)
     {
         node->get_parameter("arm_group_name", arm_group_name);
-        
+
         size_t errors = 0;
         errors += !rosparam_shortcuts::get(node, "object_pose", object_pose);
         errors += !rosparam_shortcuts::get(node, "place_pose", place_pose);
