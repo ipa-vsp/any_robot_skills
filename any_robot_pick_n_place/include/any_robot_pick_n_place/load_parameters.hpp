48ac480e430689c30bee80faed0422ab95336b36
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2023-07-13 12:59:03 +0200

    pre-commit format

--- any_robot_pick_n_place/include/any_robot_pick_n_place/load_parameters.hpp
+++ any_robot_pick_n_place/include/any_robot_pick_n_place/load_parameters.hpp
@@ -15,13 +15,13 @@
 #ifndef LOAD_PARAMETERS_HPP
 #define LOAD_PARAMETERS_HPP
 
+#include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
-#include <memory>
-#include <iostream>
 
-#include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 #include <rosparam_shortcuts/rosparam_shortcuts.h>
 
@@ -31,7 +31,7 @@ struct Parameters
     geometry_msgs::msg::Pose object_pose;
     geometry_msgs::msg::Pose place_pose;
 
-    void load(const rclcpp::Node::SharePtr & node)
+    void load(const rclcpp::Node::SharePtr &node)
     {
         node->get_parameter("arm_group_name", arm_group_name);
 
@@ -40,7 +40,6 @@ struct Parameters
         errors += !rosparam_shortcuts::get(node, "place_pose", place_pose);
         rosparam_shortcuts::shutdownIfError(errors);
     }
-
 };
 
 #endif
